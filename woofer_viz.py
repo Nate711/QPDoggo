5364f6a8e3f0ea3229a308287dd28deb92e96547
Nathan Kau <nathankau@gmail.com>
2018-12-25 17:52:15 -0800

    working trot

--- woofer_viz.py
+++ woofer_viz.py
@@ -1,41 +1,92 @@
 """
-Free joints have 7 numbers: 3D position followed by 4D unit quaternion.
+Overview:
+Applies joint-space PD controllers
+
+Note: Free joints have 7 numbers: 3D position followed by 4D unit quaternion.
 """
+
+def PropController(qpos,kp,refpos):
+	return kp*(refpos-qpos)
+
 from mujoco_py import load_model_from_path, MjSim, MjViewer, functions
 import numpy as np
 np.set_printoptions(suppress=True)
 import woofer_xml_parser
 import rotations
+import math
 
 model = load_model_from_path("woofer_out.xml")
 print(model)
 sim = MjSim(model)
 viewer = MjViewer(sim)
 
-for i in range(3000):
+
+radial_inds = np.array([2,5,8,11])
+abad_inds = np.array([0,3,6,9])
+fb_inds = np.array([1,4,7,10])
+
+# radial, then ab/ad, then extension
+joint_inds = np.concatenate([radial_inds, abad_inds, fb_inds])
+
+joint_qpos_inds = joint_inds + 7	# 7 variables for free joint position
+joint_qvel_inds = joint_inds + 6	# 6 variables for free joint velocity
+
+max_forces = np.zeros(12)
+
+for i in range(10000):
 	qpos = sim.data.qpos
+	qvel = sim.data.qvel
+
+	# sim.data.qpos[0:3] = np.array([0,0,0.6])
+	# sim.data.qpos[3:7] = np.array([1,0,0,0])
+
+	qpos_joints = qpos[joint_qpos_inds]
+	qvel_joints = qvel[joint_qvel_inds]
 
 	xyz = qpos[0:3]
-	orientation_quat = qpos[3:7]
+	orientation_quat = qpos[3:7]	
 	rotmat = rotations.quat2mat(orientation_quat)
 	rpy = rotations.mat2euler(rotmat)
 
+
+	kp = np.concatenate([500*np.ones(4), 50*np.ones(8)])
+
+	time = i/1000.0
+	freq = 2.0
+	phase = time * 2*math.pi*freq
+
+	angular_amp = 0.2
+	extension_amp = 0.1
+
+	phase0 = math.sin(phase) * angular_amp
+	phase1 = math.sin(phase + math.pi) * angular_amp
+
+	phase2 = math.sin(phase + math.pi/2) * extension_amp
+	phase3 = math.sin(phase + 3*math.pi/2) * extension_amp
+
+	refpos = np.array([phase3, phase2, phase2, phase3] + [0]*4 + [phase0, phase1, phase1, phase0])
+
+	pid_output = PropController(qpos_joints,kp,refpos)
+	pid_output = np.clip(pid_output, [-133]*4 + [-6]*8, [133]*4 + [6]*8)
+
+	max_forces = np.maximum(max_forces, np.abs(pid_output))
+
+	sim.data.ctrl[joint_inds] = pid_output
+
 	if i % 100 == 0:
 		# input("Press Enter to continue...")
-		print("Cartesian: %s"%xyz)
-		print("Rotation matrix: %s"%rotmat)
-		print("Euler angles: %s"%rpy)
-
-	radial_inds = np.array([2,5,8,11])
-	abad_inds = np.array([0,3,6,9])
-	fb_inds = np.array([1,4,7,10])
-
-	sim.data.ctrl[radial_inds] = -200
-	sim.data.ctrl[fb_inds] = 0.1
-	sim.data.ctrl[abad_inds] = 0.1
+		# print("Cartesian: %s"%xyz)
+		# print("Rotation matrix: %s"%rotmat)
+		# print("Euler angles: %s"%rpy)
+		# print("Control outs: %s"%pid_output)
+		print("Max: %s"%max_forces)
 
 	sim.step()
 	viewer.render()
+
+
+
+
 
 
 
--- woofer_xml_parser.py
+++ woofer_xml_parser.py
@@ -13,18 +13,20 @@ doggo_solimp1 = 0.96					# contact parameter
 doggo_solimp2 = 0.96					# contact parameter
 
 ## Geometry params ##
-woofer_leg_radius = 0.020 				# radius of leg capsule
+woofer_leg_radius = 0.02 				# radius of leg capsule
 doggo_friction = 1.5 					# friction between legs and ground
 woofer_half_size = "0.33 0.088 0.046"	# half-size of body box
 woofer_leg_fb = 0.23 					# front-back distance from center line to leg axis
 woofer_leg_lr = 0.175 					# left-right distance from center line to leg plane
+woofer_leg_geom = "0 0 0 0 0 -0.3"
 
 ## Joint params ##
 woofer_joint_range = "-3 3"				# joint range in rads for angular joints
 woofer_joint_force_range = "-12 12"		# force range for ab/ad and forward/back angular joints
 woofer_ext_force_range = "-200 200"		# force range for radial/extension joint
 woofer_ext_range = "-0.18 0.18"			# joint range for radial/extension joint
-woofer_rad_damping = 1.0 				# damping on radial/extension joint [Nm/rad/s]
+woofer_rad_damping = 2.0 				# damping on radial/extension joint [N/m/s]
+woofer_joint_damping = 2.0				# damping on ab/ad and f/b angular joints [Nm/rad/s]
 
 
 ###### FILE PATHS  #####
@@ -61,12 +63,16 @@ filedata = filedata.replace('woofer_ext_range', str(woofer_ext_range))
 filedata = filedata.replace('woofer_joint_range', str(woofer_joint_range))
 filedata = filedata.replace('woofer_joint_force_range', str(woofer_joint_force_range))
 filedata = filedata.replace('woofer_rad_damping', str(woofer_rad_damping))
+filedata = filedata.replace('woofer_joint_damping', str(woofer_joint_damping))
+
 
 # Geometry specs
 filedata = filedata.replace('woofer_leg_radius', str(woofer_leg_radius))
 filedata = filedata.replace('woofer_half_size', str(woofer_half_size))
 filedata = filedata.replace('woofer_leg_fb', str(woofer_leg_fb))
 filedata = filedata.replace('woofer_leg_lr', str(woofer_leg_lr))
+filedata = filedata.replace("woofer_leg_geom", str(woofer_leg_geom))
+
 
 # Write the xml file
 with open(out_file, 'w') as file:
